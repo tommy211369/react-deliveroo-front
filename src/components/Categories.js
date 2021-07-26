1adcb58ef1d7f7f63f4401d5cd429d2ff6567da8
tommy211369 <tommy.thongrasamy@gmail.com>
2021-07-26 20:59:43 +0200

    changed data

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -5,11 +5,9 @@ import Category from "./Category";
 const Categories = ({ data }) => {
   return (
     <div className="categories">
-      {data.categories
-        .splice(data.categories.length - 6)
-        .map((category, index) => {
-          return <Category category={category} index={index} />;
-        })}
+      {data.categories.map((category, index) => {
+        return <Category category={category} index={index} />;
+      })}
     </div>
   );
 };
