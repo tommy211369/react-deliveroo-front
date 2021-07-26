554ef53fd72d9b7aa5100d5034322a76bd24176c
tommy211369 <tommy.thongrasamy@gmail.com>
2021-07-26 15:07:12 +0200

    add useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
-  return (
-    <div>
-      Hello from <a href="https://www.lereacteur.io">Le Reacteur !</a>
-    </div>
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get("http://localhost:3200/");
+      setData(response.data);
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  return isLoading ? (
+    <span>En cours de chargement ...</span>
+  ) : (
+    <h2>{data.restaurant.name}</h2>
   );
 }
 
