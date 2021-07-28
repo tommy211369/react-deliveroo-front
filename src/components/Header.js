e7cfb848d6f3beb8bfd370acecd69ebabd8bfdcd
tommy211369 <tommy.thongrasamy@gmail.com>
2021-07-28 09:27:55 +0200

    add cart to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,14 @@ import React from "react";
 import logo from "../img/logo-deliveroo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const Header = ({ cart, setCart, displayCart, setDisplayCart }) => {
+const Header = ({ cart, displayCart, setDisplayCart }) => {
+  const arrayCart = Object.keys(cart);
   return (
     <div className="header">
       <div className="container">
         <img src={logo} alt="logo deliveroo" />
         <div className="header-cart">
-          {cart.length > 0 && <span>{cart.length}</span>}
+          {arrayCart.length > 0 && <span>{arrayCart.length}</span>}
           <FontAwesomeIcon icon="shopping-cart" className="cart-menu" />
         </div>
       </div>
