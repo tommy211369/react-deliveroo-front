b051595edf583b9bee4f70ce24ab43ecb8ef17cd
tommy211369 <tommy.thongrasamy@gmail.com>
2021-07-29 19:32:16 +0200

    header cart

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,9 @@
 import React from "react";
 import logo from "../img/logo-deliveroo.png";
+import MenuCart from "./MenuCart";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const Header = ({ cart, displayCart, setDisplayCart }) => {
+const Header = ({ cart, setCart, displayCart, setDisplayCart }) => {
   const cartArray = Object.keys(cart);
 
   const newCart = { ...cart };
@@ -19,8 +20,23 @@ const Header = ({ cart, displayCart, setDisplayCart }) => {
         <img src={logo} alt="logo deliveroo" />
         <div className="header-cart">
           {totalQuantity > 0 && <span>{totalQuantity}</span>}
-          <FontAwesomeIcon icon="shopping-cart" className="cart-menu" />
+          <FontAwesomeIcon
+            icon="shopping-cart"
+            className="cart-menu"
+            onClick={() => {
+              setDisplayCart(!displayCart);
+            }}
+          />
         </div>
+
+        {displayCart && (
+          <MenuCart
+            cart={cart}
+            setCart={setCart}
+            displayCart={displayCart}
+            setDisplayCart={setDisplayCart}
+          />
+        )}
       </div>
     </div>
   );
---
+++ src/components/MenuCart.js
@@ -0,0 +1,101 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const MenuCart = ({ cart, setCart, displayCart, setDisplayCart }) => {
+  let total = 0;
+
+  // change object into array
+  const cartArray = Object.keys(cart); // array of cart keys (ids)
+  const newCart = { ...cart }; // copy of cart (meal objects)
+
+  for (let i = 0; i < cartArray.length; i++) {
+    console.log(cartArray[i]); // one id
+    // newCart[cartArray[i]] : one meal
+    total += Number(
+      newCart[cartArray[i]].price * newCart[cartArray[i]].quantity
+    );
+  }
+
+  return (
+    <div className="basket-header">
+      {cartArray.length > 0 ? (
+        <div className="basket-on">
+          <div className="valid valid-on">Valider votre panier</div>
+
+          {/* array of ids */}
+          {cartArray.map((mealId, id) => {
+            return (
+              <div key={id} className="cart-meals">
+                <div className="counter">
+                  <span
+                    onClick={() => {
+                      const newCart = { ...cart }; // copy of cart (meal objects)
+
+                      newCart[mealId].quantity--;
+                      if (newCart[mealId].quantity === 0) {
+                        cartArray.splice(id, 1);
+                        delete newCart[mealId];
+                      }
+
+                      setCart(newCart);
+                    }}
+                  >
+                    <FontAwesomeIcon
+                      icon="minus-circle"
+                      className="counter-icon"
+                    />
+                  </span>
+
+                  <span className="quantity">{newCart[mealId].quantity}</span>
+
+                  <span
+                    onClick={() => {
+                      const newCart = { ...cart };
+                      newCart[mealId].quantity++;
+                      setCart(newCart);
+                    }}
+                  >
+                    <FontAwesomeIcon
+                      icon="plus-circle"
+                      className="counter-icon"
+                    />
+                  </span>
+                </div>
+
+                <div className="cart-meal-details">
+                  <p>{newCart[mealId].name}</p>
+                  <span>{newCart[mealId].price} €</span>
+                </div>
+              </div>
+            );
+          })}
+
+          {/* SUBTOTAL & SHIPPING COST*/}
+          <div className="taxes-sous-total">
+            <div>
+              <p>Sous-total</p>
+              <span>{total.toFixed(2)} €</span>
+            </div>
+            <div>
+              <p>Frais de livraison</p>
+              <span>{parseFloat(2.5).toFixed(2)} €</span>
+            </div>
+          </div>
+
+          {/* TOTAL */}
+          <div className="total">
+            <p>Total</p>
+            <span>{(total + parseFloat(2.5)).toFixed(2)} €</span>
+          </div>
+        </div>
+      ) : (
+        <div className="basket-off">
+          <div className="valid valid-off">Valider votre panier</div>
+          <p className="empty-basket">Votre panier est vide</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default MenuCart;
